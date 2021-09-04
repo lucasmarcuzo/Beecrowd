0c7d7583f218c4f4e050564ebf9d34d56f7ecd28
Lucas Marcuzo <lucas.marcuzo@hotmail.com>
2021-09-04 13:37:56 -0300

    add 2510

---
+++ Desafios em C#/2510 - Batmain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _2510___Batmain
+{
+    public class URI{
+
+        public static void Main(string[] args) { 
+            
+            int t = int.Parse(Console.ReadLine());
+            
+            int x = 0; 
+            while(x < t) {
+                
+                string vilao = Console.ReadLine();
+                Console.WriteLine("Y"); 
+                x++;
+            }    
+
+        }
+
+    }
+
+
+
+
+}
---
+++ Desafios em Java/2510_Batmain/Main2510.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main2510 {
+    
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        
+        int t = scan.nextInt();
+        
+        int x = 0; 
+        while(x < t) {
+            
+        	String vilao = scan.next();
+        	System.out.println("Y"); 
+        	x++;
+        }       
+    }
+}
